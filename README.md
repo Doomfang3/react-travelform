57373a9a7e7de60d45b7dd11b2eb75ca282ed9cc
Mathieu Saubin <saubinm@free.fr>
2019-04-05 10:57:55 +0200

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-This exercise is adapted from the V School curriculum on vanilla JS forms:
-https://coursework.vschool.io/travel-form/
---
+++ precache-manifest.617c476a4b3d08827dfe542759a30de3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f96f0ad9b9aa21729443",
+    "url": "/react-travelform/yarn%20add%20gh-pages/static/js/runtime~main.8edaf339.js"
+  },
+  {
+    "revision": "40a450e31dae76dc8fd3",
+    "url": "/react-travelform/yarn%20add%20gh-pages/static/js/main.221a2b8c.chunk.js"
+  },
+  {
+    "revision": "5aef0e42fd9401a12e67",
+    "url": "/react-travelform/yarn%20add%20gh-pages/static/js/2.ff73aee1.chunk.js"
+  },
+  {
+    "revision": "40a450e31dae76dc8fd3",
+    "url": "/react-travelform/yarn%20add%20gh-pages/static/css/main.6b1b72b9.chunk.css"
+  },
+  {
+    "revision": "38e5089f3a393f8a36acb6e0a013e5bf",
+    "url": "/react-travelform/yarn%20add%20gh-pages/index.html"
+  }
+];
